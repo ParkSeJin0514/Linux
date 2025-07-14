05ccd2826aa80904ef5d8b10f028b75ecf89e2ed
ParkSeJin <bjtanker_@naver.com>
2025-07-14 17:59:33 +0900

    07.14 학습 내용

--- codes/first.sh
+++ codes/first.sh
@@ -1,3 +1,10 @@
-#first
-#second
-~$ ls
+[parksejin@localhost Downloads]$ pwd
+/home/parksejin/Downloads
+[parksejin@localhost Downloads]$ cd /home/parksejin/Downloads
+[parksejin@localhost Downloads]$ pwd
+/home/parksejin/Downloads
+[parksejin@localhost Downloads]$ ls
+[parksejin@localhost Downloads]$ pwd
+/home/parksejin/Downloads
+[parksejin@localhost Downloads]$ cd
+[parksejin@localhost ~]$ 
